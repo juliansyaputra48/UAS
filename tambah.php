a4bbcbf8fc2d07dd02f052a78415582237780b68
Juliansyaputra <92143455+juliansyaputra48@users.noreply.github.com>
2022-01-18 10:26:17 -0800

    Add files via upload

---
+++ tambah.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+    header('location: login.php');
+}
+
+?>
+<!doctype html>
+<html>
+<head>
+    <title>Tambah Data Artikel</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+     <h2 class="alert alert-success" class="logo">
+            <img src="foto/ubg.png">
+
+            UNIVERSITAS BUMIGORA MATARAM    
+        </h2>
+
+    <?php
+    require 'setting.php';
+
+    if (isset($_POST['simpan'])) {
+        $tnim = $_POST['tnim'];
+        $tnama = $_POST['tnama'];
+        $talamat = $_POST['talamat'];
+        $tprogram = $_POST['tprogram'];
+
+        $sql = "INSERT INTO mahasiswa VALUES (NULL, '$tnim', '$tnama', '$talamat', '$tprogram')";
+
+        $query = mysqli_query($koneksi, $sql);
+        if ($query) {
+            header('location: index.php');
+        } else {
+            echo 'Query Error : ' . mysqli_error($koneksi);
+        }
+    }
+    ?>
+    <div class="card mt-4">
+        <form action="" method="post">
+
+            <div class="mb-2">
+                <label>Nim</label>
+                <input required type="text" name="tnim" class="form-control">
+            </div>
+
+            <div class="mb-2">
+                <label>Nama</label>
+                <input type="text" name="tnama" class="form-control">
+            </div>
+
+            <div class="mb-2">
+                <label>Alamat</label>
+                <textarea name="talamat" class="form-control" cols="30" rows="5"></textarea>
+            </div>
+
+            <div class="mb-4">
+                <label>Program</label>
+                <input type="text" name="tprogram" class="form-control">
+            </div>
+
+            
+        </div>
+
+            <input type="submit" name="simpan" value="Simpan" class="btn btn-success">
+            <a href="index.php" class="btn btn-secondary float-end">Kembali</a>
+        </form>
+    </div>
+</div>
+</body>
+</html>
